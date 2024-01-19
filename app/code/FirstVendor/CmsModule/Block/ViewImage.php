997500bb9bf6ccd6c1a06c4d8e38e7ceec74e0f5
AlenEglobe <alen.george@eglobeits.com>
2024-01-19 11:29:34 +0530

    refactor : create new cms page  , add image , add footerlink

--- app/code/FirstVendor/CmsModule/Block/ViewImage.php
+++ app/code/FirstVendor/CmsModule/Block/ViewImage.php
@@ -2,16 +2,8 @@
 
 namespace FirstVendor\CmsModule\Block;
 
-class ViewImage extends \Magento\Framework\View\Element\Template
+use Magento\Framework\View\Element\Template;
+
+class ViewImage extends Template
 {
-    /**
-     * @param \Magento\Framework\View\Element\Template\Context $context
-     * @param array $data
-     */
-    public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context,
-        array $data = []
-    ) {
-        parent::__construct($context, $data);
-    }
 }
---
+++ app/code/FirstVendor/CmsModule/Setup/Patch/Data/TheCmsPage.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace FirstVendor\CmsModule\Setup\Patch\Data;
+
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchVersionInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Cms\Model\PageFactory;
+
+
+class TheCmsPage implements DataPatchInterface, PatchVersionInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var PageFactory
+     */
+    private $pageFactory;
+
+    /**
+     * AddNewCmsPage constructor.
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        PageFactory $pageFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->pageFactory = $pageFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $pageData = [
+            'title' => 'My Cms Page',
+            'page_layout' => '1column',
+            'meta_keywords' => 'Page keywords',
+            'meta_description' => 'Page description',
+            'identifier' => 'cms-page',
+            'content_heading' => 'CMS page',
+            'content' => '<div class="main-cms-content">Content goes here for My cms page. CMS Page create using Programmatically {{block class="FirstVendor\CmsModule\Block\ViewImage" template="FirstVendor_CmsModule::image.phtml"}}</div>',
+            'layout_update_xml' => '',
+            'url_key' => 'cms-page',
+            'is_active' => 1,
+            'stores' => [0], // store_id comma separated
+            'sort_order' => 0
+        ];
+
+        $this->moduleDataSetup->startSetup();
+        /* Save CMS Page logic */
+        $this->pageFactory->create()->setData($pageData)->save();
+        $this->moduleDataSetup->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getVersion()
+    {
+        return '2.0.0';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
