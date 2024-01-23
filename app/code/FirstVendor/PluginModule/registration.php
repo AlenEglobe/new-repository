31609d1db13cbda316f606b009cdbd34a89a1eaa
AlenEglobe <alen.george@eglobeits.com>
2024-01-23 14:06:22 +0530

    refactor: create a plugin to  set the quantity to 10 using before plugin.

--- app/code/FirstVendor/PluginModule/registration.php
+++ app/code/FirstVendor/PluginModule/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'FirstVendor_CmsModule',
+    'FirstVendor_PluginModule',
     __DIR__
 );
